08d2e5fbf404b6d8179fbd11f6bde5cf22aa2dc2
shiftleino <shiftleino@gmail.com>
2024-03-22 11:03:02 +0200

    Add evaluation framework for transformers models

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -2,20 +2,67 @@ import json
 import numpy as np
 import pandas as pd
 from openai import OpenAI
+from sentence_transformers import SentenceTransformer
 
 
 class IREvaluator:
-    def __init__(self):
+    """Evaluator class for Information Retrieval (IR) tasks. The class provides methods
+    to evaluate the performance of course embeddings on an evaluation set using the Normalized
+    Discounted Cumulative Gain (NDCG) metric. The class supports evaluation of both OpenAI
+    and SentenceTransformer embeddings.
+    """
+    def __init__(self, eval_path: str):
+        self.eval_path = eval_path
         self.eval_set = self._load_evaluation_data()
         self.top_n_courses_openai = {}
+        self.top_n_courses_transformers = {}
 
-    def _load_evaluation_data(self):
-        with open("eval_set.json", "r") as f:
+    def _load_evaluation_data(self) -> dict:
+        """Loads the evaluation set from the given evaluation set path.
+
+        Returns:
+            dict: The evaluation set as a dictionary.
+        """
+        with open(self.eval_path, "r") as f:
             return json.load(f)
 
-    def evaluate_openai_embeddings_ir(self) -> dict[str, float]:
+    def evaluate_transformers_embeddings_ir(self, model: str, embeddings_filepath: str) -> dict[str, float]:
+        """Evaluates the SentenceTransformer course embeddings on the evaluation set using as metrics
+        the average of Normalized Discounted Cumulative Gain (NDCG) of the evaluation queries.
+
+        Args:
+            model (str): The name of the SentenceTransformer model to use.
+            embeddings_filepath (str): The path to the pickle file containing the course embeddings.
+
+        Returns:
+            dict[str, float]: A dictionary containing the metric scores for the evaluation set.
+        """
+        model = SentenceTransformer(model)
+        embeddings, course_codes = self._load_course_embeddings(embeddings_filepath)
+
+        ndcgs = []
+        for query in self.eval_set["queries"]:
+            query_embedding = model.encode(query["query"])
+            top_n_idx = self._vector_search(query_embedding, embeddings)
+            top_n_courses = course_codes[top_n_idx]
+            self.top_n_courses_transformers[query["query"]] = top_n_courses
+            ndcg = self.calculate_ndcg(ground_truths=query["answers"], predictions=top_n_courses)
+            ndcgs.append(ndcg)
+        
+        return {"ndcg": np.mean(ndcgs)}
+
+    def evaluate_openai_embeddings_ir(self, embeddings_filepath: str) -> dict[str, float]:
+        """Evaluates the OpenAI course embeddings on the evaluation set using as metrics
+        the average of Normalized Discounted Cumulative Gain (NDCG) of the evaluation queries.
+
+        Args:
+            embeddings_filepath (str): The path to the pickle file containing the course embeddings.
+
+        Returns:
+            dict[str, float]: A dictionary containing the metric scores for the evaluation set.
+        """
         client = OpenAI()
-        embeddings, course_codes = self._load_openai_course_embeddings()
+        embeddings, course_codes = self._load_course_embeddings(embeddings_filepath)
 
         ndcgs = []
         for query in self.eval_set["queries"]:
@@ -28,19 +75,57 @@ class IREvaluator:
 
         return {"ndcg": np.mean(ndcgs)}
 
-    def _load_openai_course_embeddings(self):
-        openai_embeddings_df = pd.read_pickle("embeddings_pretranslated_openai-small.pkl")
-        openai_embeddings = openai_embeddings_df["embedding"].to_list()
-        openai_embeddings = np.vstack(openai_embeddings)
-        course_codes = openai_embeddings_df["course_code"]
-        return openai_embeddings, course_codes
+    def _load_course_embeddings(self, filepath: str) -> tuple[np.ndarray, pd.Series]:
+        """Loads the course embeddings and course codes from a given file. File
+        must be a pickle file of the format where the first column is the course 
+        code and the second column is the embedding of the corresponding course as 
+        numpy array.
 
-    def _get_openai_embedding(self, text, client, model="text-embedding-3-small"):
+        Args:
+            filepath (str): The path to the pickle file containing the course embeddings.
+
+        Returns:
+            tuple[np.ndarray, pd.Series]: A tuple containing the course embeddings as a 
+            numpy array and the course codes as Pandas Series.
+        """
+        embeddings_df = pd.read_pickle(filepath)
+        embeddings = embeddings_df["embedding"].to_list()
+        embeddings = np.vstack(embeddings)
+        course_codes = embeddings_df["course_code"]
+        return embeddings, course_codes
+
+    def _get_openai_embedding(self, text: str, client: OpenAI, model="text-embedding-3-small") -> np.ndarray:
+        """Get the OpenAI embedding for a given text using the OpenAI API. You need
+        to have set up the OpenAI API key in your environment variables to use this.
+        Check the OpenAI API documentation for more information on how to set up the
+        API key.
+
+        Args:
+            text (str): Text to be encoded.
+            client (openai.OpenAI): OpenAI client object.
+            model (str, optional): Name of the model used for embeddings. 
+            Defaults to "text-embedding-3-small".
+
+        Returns:
+            np.ndarray: The embedding of the text.
+        """
         text = text.replace("\n", " ")
         embedding = client.embeddings.create(input = [text], model=model).data[0].embedding
         return embedding
     
-    def _vector_search(self, query, embeddings, top_n=5):
+    def _vector_search(self, query: np.ndarray, embeddings: np.ndarray, top_n=5) -> np.ndarray:
+        """Searches for the top_n most similar vectors to the query vector in the
+        embeddings matrix using the dot product as similarity measure.
+
+        Args:
+            query (np.ndarray): The query embedding vector.
+            embeddings (np.ndarray): The embeddings matrix.
+            top_n (int, optional): Number of most similar instances to search. 
+            Defaults to 5.
+
+        Returns:
+            np.ndarray: The indices of the top_n most similar vectors.
+        """
         scores = np.dot(embeddings, query)
         top_n_idx = scores.argsort()[::-1][:top_n]
         return top_n_idx
